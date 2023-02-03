ff8fb63a60124b8ccd5ba7f9b9d7c87337d49d08
Danil Dergachev <ddergachev2001@gmail.com>
2023-02-03 17:23:05 +0300

    main code wrapped in a function

--- main_code/main.py
+++ main_code/main.py
@@ -1,27 +1,3 @@
-from utils import pull_out_id, get_json
+from utils import main_function
 
-file = get_json()
-list_of_id = pull_out_id()
-for id_number in list_of_id:
-    for i in file:
-        if len(i) != 0 and i['id'] == id_number:
-            time_data = i['date'].split('T')
-            date_dash = time_data[0].split('-')
-            required_date = f"{date_dash[2]}.{date_dash[1]}.{date_dash[0]}"
-            disc = i['description']
-            if 'from' in i:
-                from_ = i['from'].split(' ')
-                if len(from_) == 3:
-                    from_num = from_[2]
-                    abs_from = f"{from_[0]} {from_[1]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-                else:
-                    from_num = from_[1]
-                    abs_from = f"{from_[0]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-            else:
-                abs_from = ''
-            to = i['to'].split(' ')
-            num_of_trans = to[1]
-            summ = i["operationAmount"]["amount"]
-            cur = i["operationAmount"]["currency"]["name"]
-            print(f"{required_date} {disc}\n{abs_from}"
-                  f"{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n")
+main_function()
--- main_code/utils.py
+++ main_code/utils.py
@@ -1,15 +1,41 @@
 import requests
 
 
-def get_json():
+def main_function():
+    file = get_json()
+    list_of_id = pull_out_id()
+    for id_number in list_of_id:
+        for i in file:
+            if len(i) != 0 and i['id'] == id_number:
+                time_data = i['date'].split('T')
+                date_dash = time_data[0].split('-')
+                required_date = f"{date_dash[2]}.{date_dash[1]}.{date_dash[0]}"
+                disc = i['description']
+                if 'from' in i:
+                    from_ = i['from'].split(' ')
+                    if len(from_) == 3:
+                        from_num = from_[2]
+                        abs_from = f"{from_[0]} {from_[1]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
+                    else:
+                        from_num = from_[1]
+                        abs_from = f"{from_[0]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
+                else:
+                    abs_from = ''
+                to = i['to'].split(' ')
+                num_of_trans = to[1]
+                summ = i["operationAmount"]["amount"]
+                cur = i["operationAmount"]["currency"]["name"]
+                print(f"{required_date} {disc}\n{abs_from}"
+                      f"{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n")
 
+
+def get_json():
     file = requests.get("https://www.jsonkeeper.com/b/FGAS")
     file = file.json()
     return file
 
 
 def pull_out_id():
-
     list_of_id = []
     dates = []
     dict_of_dates = {}

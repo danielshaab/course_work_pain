85f61c6910d308b0ccfd554e9efc67fd835dccb9
Danil Dergachev <ddergachev2001@gmail.com>
2023-02-13 11:13:43 +0300

    added tests for main customized utils etc

--- main_code/utils.py
+++ main_code/utils.py
@@ -1,47 +1,19 @@
 import requests
 
 
-def main_function():
-    file = get_json()
-    list_of_id = pull_out_id()
-    for id_number in list_of_id:
-        for i in file:
-            if len(i) != 0 and i['id'] == id_number:
-                time_data = i['date'].split('T')
-                date_dash = time_data[0].split('-')
-                required_date = f"{date_dash[2]}.{date_dash[1]}.{date_dash[0]}"
-                disc = i['description']
-                if 'from' in i:
-                    from_ = i['from'].split(' ')
-                    if len(from_) == 3:
-                        from_num = from_[2]
-                        abs_from = f"{from_[0]} {from_[1]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-                    else:
-                        from_num = from_[1]
-                        abs_from = f"{from_[0]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
-                else:
-                    abs_from = ''
-                to = i['to'].split(' ')
-                num_of_trans = to[1]
-                summ = i["operationAmount"]["amount"]
-                cur = i["operationAmount"]["currency"]["name"]
-                print(f"{required_date} {disc}\n{abs_from}"
-                      f"{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n")
-
-
-def get_json():
-    file = requests.get("https://www.jsonkeeper.com/b/FGAS")
+def getting_json_from_web(link):
+    """Ф-я получает список словарей по ссылке"""
+    file = requests.get(link)
     file = file.json()
     return file
 
 
-def pull_out_id():
+def finding_ides(link, quantity=5, sort_type=True):
+    """Ф-я находит id последних операций и возвращает их в правильном порядке"""
     list_of_id = []
     dates = []
     dict_of_dates = {}
-
-    file = get_json()
-
+    file = getting_json_from_web(link)
     for index in file:
         if len(index) != 0 and index['state'] == "EXECUTED":
             date_month_num = index['date'].split('T')
@@ -51,9 +23,25 @@ def pull_out_id():
             dates.append(full_date)
             dict_of_dates[full_date] = index_id
 
-    dates.sort(reverse=True)
-    dates = dates[0:5]
+    dates.sort(reverse=sort_type)
+    dates = dates[0:quantity]
     while len(list_of_id) < 5:
         for ides in dates:
             list_of_id.append(dict_of_dates[ides])
+    for ides in dates:
+        list_of_id.append(dict_of_dates[ides])
     return list_of_id
+
+
+def key_in_list(key, list_):
+    if key in list_:
+        from_ = list_['from'].split(' ')
+        if len(from_) == 3:
+            from_num = from_[2]
+            abs_from = f"{from_[0]} {from_[1]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
+        else:
+            from_num = from_[1]
+            abs_from = f"{from_[0]} {from_num[:4]} {from_num[4:6]}** **** {from_num[-4:]} -> "
+    else:
+        abs_from = ''
+    return abs_from

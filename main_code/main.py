27e8d8260b121454eb9c507387e9a88fa0ccbcb7
Danil Dergachev <ddergachev2001@gmail.com>
2023-02-13 11:13:43 +0300

    added tests for main customized utils etc

--- main_code/main.py
+++ main_code/main.py
@@ -1,3 +1,22 @@
-from utils import main_function
+from utils import finding_ides, getting_json_from_web, key_in_list
 
-main_function()
+link = "https://www.jsonkeeper.com/b/FGAS"
+
+file = getting_json_from_web(link)
+list_of_id = finding_ides(link)
+for idefic in list_of_id:
+    """Перебираем каждый id и находим по нему всю информацию"""
+    for item in file:
+        if len(item) != 0 and item['id'] == idefic:
+            time_data = item['date'].split('T')
+            date_ = time_data[0].split('-')
+            normal_date = f"{date_[2]}.{date_[1]}.{date_[0]}"
+            disc = item['description']
+            abs_from = key_in_list('from', item)
+            to = item['to'].split(' ')
+            num_of_trans = to[1]
+            summ = item["operationAmount"]["amount"]
+            cur = item["operationAmount"]["currency"]["name"]
+            """собираем оставшуюся необходимую информацию и делаем вывод транзакции"""
+            print(f"{normal_date} {disc}\n{abs_from}"
+                  f"{to[0]} **{num_of_trans[-4:]}\n{summ} {cur}\n")
---
+++ tests/tests_for_main.py
@@ -0,0 +1,16 @@
+from main_code.utils import finding_ides
+
+test_link = "https://www.jsonkeeper.com/b/MBOG"
+main_link = "https://www.jsonkeeper.com/b/FGAS"
+
+
+def test_getting_json_from_web():
+    pass
+
+
+def test_for_finding_ides():
+    assert finding_ides(main_link, 10, False) == {317987878, 634356296, 260972664, 147815167, 893507143, 893507143,
+                                                  286706711, 988276204, 361044570, 921286598}
+    assert finding_ides(main_link, 2, True) == [863064926, 114832369]
+    assert finding_ides(main_link, 1, False) == [317987878]
+    assert finding_ides(main_link) == [863064926, 114832369, 154927927, 482520625, 801684332]
